96d9a07bb34aaafae3453290152cf02cffbe7717
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2018-06-16 01:04:22 +0900

    Update ERC721TestToken to purely inherit ERC721Token

--- contracts/ERC721TestToken.sol
+++ contracts/ERC721TestToken.sol
@@ -3,21 +3,7 @@ pragma solidity ^0.4.21;
 import "../submodules/openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 contract ERC721TestToken is ERC721Token {
-    uint256 internal nextTokenId = 0;
 
-    constructor() public ERC721Token("MyToken", "MTKN") {}
+    constructor(string _name, string _symbol) ERC721Token(_name, _symbol) public {}
 
-    function mint() external {
-        uint256 tokenId = nextTokenId;
-        nextTokenId = nextTokenId.add(1);
-        super._mint(msg.sender, tokenId);
-    }
-
-    function setTokenURI(uint256 _tokenId, string _message) external onlyOwnerOf(_tokenId) {
-        super._setTokenURI(_tokenId, _message);
-    }
-
-    function burn(uint256 _tokenId) external onlyOwnerOf(_tokenId) {
-        super._burn(msg.sender, _tokenId);
-    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var ERC721TestToken = artifacts.require("ERC721TestToken");
 
 module.exports = async (deployer, network) => {
-  await deployer.deploy(ERC721TestToken);
+  await deployer.deploy(ERC721TestToken, "KomyTestToken", "KMT");
   console.log('ERC721TestToken.address was deployed');
   console.log(ERC721TestToken.address);
 };
