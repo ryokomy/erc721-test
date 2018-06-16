e16b09e8cb887d99a775ada14f8aa166f82f27cb
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2018-06-16 15:49:53 +0900

    Update tools/exec_mints.js

--- tools/exec_mints.js
+++ tools/exec_mints.js
@@ -1,24 +1,20 @@
 const Kyuzan721Token = artifacts.require('Kyuzan721Token')
 
-let to = web3.eth.accounts[0]
-let tokenId = 1
-let name = 'ryokomy'
-let company = 'kyuzan'
-
-let members = [
-    {to: web3.eth.accounts[0], tokenId: 1, name: 'takumi', company: 'kyuzan'},
-    {to: web3.eth.accounts[0], tokenId: 2, name: 'komy', company: 'kyuzan'},
-    {to: web3.eth.accounts[0], tokenId: 3, name: 'shintaro', company: 'yahoo'},
-    {to: web3.eth.accounts[0], tokenId: 4, name: 'tomohiro', company: 'berlin'},
-    {to: web3.eth.accounts[0], tokenId: 5, name: 'tomonari', company: 'recruit'}
-]
-
-
 module.exports = async (callback) => {
-    // perform actions
+    // let account = web3.eth.accounts[0]
+    let account = '0xF9D077aCcDcCf8Bb7b107F7E319a249BE592fBeC'
+    let members = [
+        {to: account, tokenId: 1, name: 'takumi', company: 'kyuzan'},
+        {to: account, tokenId: 2, name: 'komy', company: 'kyuzan'},
+        {to: account, tokenId: 3, name: 'shintaro', company: 'yahoo'},
+        {to: account, tokenId: 4, name: 'tomohiro', company: 'berlin'},
+        {to: account, tokenId: 5, name: 'tomonari', company: 'recruit'}
+    ]
+    console.log(Kyuzan721Token.address)
     let instance = await Kyuzan721Token.deployed();
+    let results = [];
     for (member of members) {
-        let result = await instance.mint(member.to, member.tokenId, member.name, member.company);
-        console.log(result);
+        results.push(instance.mint(member.to, member.tokenId, member.name, member.company))
     }
+    await Promise.all(results);
 };
--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var local_config = require("./local/config.js");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -6,6 +9,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: 5777
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(local_config.mnemonic, "https://ropsten.infura.io/" + local_config.infura_access_token);
+      },
+      network_id: 3,
+      gas: 6000000,
+      gasPrice: 5000000000
     }
   },
   solc: {
